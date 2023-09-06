97175cfa74bb5761bbe98d7410bbd9e37be39fed
Tabata Souto <tabatac.souto@gmail.com>
2023-09-06 15:15:49 -0300

    chore: finalização página content

---
+++ src/app/database/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo:
+      'https://cdn1.epicgames.com/offer/6f43ab8025ad42d18510aa91e9eb688b/EGS_FINALFANTASYVIIREMAKEINTERGRADE_SquareEnix_S1_2560x1440-85f829541a833442eaace75d02e0f07d',
+    title: 'Final Fantasy 7 REMAKE: Novo jogo',
+    description:
+      'A história se passa na metrópole de Midgar e acompanha o mercenário Cloud Strife, que se junta a um grupo ecoterrorista em uma tentativa de impedir que a poderosa megacorporação Shinra use a essência vital do planeta como fonte de energia.',
+  },
+  {
+    id: 2,
+    photo:
+      'https://assetsio.reedpopcdn.com/Final-Fantasy-7-Remake-Review-Screenshot-1-04052020.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp',
+    title: 'Entenda a jogabilidade',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: 3,
+    photo:
+      'https://meups.com.br/wp-content/uploads/2022/07/final-fantasy-vii-remake.jpg',
+    title: 'História foi alterada para manter o interesse dos jogadores',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: 4,
+    photo:
+      'https://files.tecnoblog.net/wp-content/uploads/2021/06/final-fantasy-7-intermission-1536x864.jpeg',
+    title: 'Vale a pena jogar: Final Fantasy 7 Intergrade + Intermission',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+];
